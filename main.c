edc17f2636a24c6bac5bf9cdfa78e327d2c961ec
Zheng Ling <rushling@hormail.com>
2013-01-01 15:58:23 +0800

    modify it from dev branch

--- main.c
+++ main.c
@@ -1,9 +1,1 @@
-this is a main c program developed in dev branch
-add something in main branch
-add something from ddmivm24
-add something from zling2
-add something in main branch
-modify it in master branch
-modify it in master branch 1
-modify it in dev branch
-modify it in dev branch 1
+modify it from dev branch
